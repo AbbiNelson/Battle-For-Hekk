296a5cbee8c0923434d5552ad58fe95254ee67fd
MUKILTEO\1725608 <talonlee44@gmail.com>
2026-01-21 10:22:53 -0800

    WP text

--- Assets/Talon/TalonScripts/StartGameA.cs
+++ Assets/Talon/TalonScripts/StartGameA.cs
@@ -3,12 +3,40 @@ using UnityEngine.InputSystem;
 
 public class StartGameA : MonoBehaviour
 {
+    public GameObject text;
+    bool isTextActive = true;
+    public float delay = 1f;
     public void OnPressA(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
         {
             Debug.Log("Start Game A Pressed");
             // Add your logic to start Game A here
+        }
+    }
+    public void OnPressB(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            Debug.Log("Exit Game B Pressed");
+            // Add your logic to start Game B here
+            Application.Quit();
+        }
+    }
+    private void Update()
+    {
+        delay -= Time.deltaTime;
+        if (isTextActive && delay == 0f)
+        {
+            text.SetActive(true);
+            isTextActive = false;
+            delay = 1f;
+        }
+        else if (isTextActive == false && delay == 0f)
+        {
+            text.SetActive(false);
+            isTextActive = true;
+            delay = 1f;
         }
     }
 }
