3b31382a29329def66cb5f49f9c37dbfaf50cee1
MUKILTEO\1725219 <george.chmykhalo@gmail.com>
2026-01-23 09:52:50 -0800

    Multiplayer tokens done

---
+++ Assets/Tile-Editor/PlayerSelection.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerSelection : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float speed = 15f;
+    public PlayerHandler pH;
+    void Start()
+    {
+        pH = GameObject.Find("PlayerHandler").GetComponent<PlayerHandler>();
+        rb2d = GetComponent<Rigidbody2D>();
+        pH.playerCount++;
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = pH.selectedSprite;
+    }
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        rb2d.linearVelocity = ctx.ReadValue<Vector2>() * new Vector2(speed, speed);
+    }
+}
