84546e69c3df0fb5ddfd5959e78df5993f7d7d79
MUKILTEO\1725468 <ceballosg365@gmail.com>
2026-01-27 10:13:28 -0800

    no message

--- Assets/Gustavo/Fireball molotov whatever this is/FireballFire.cs
+++ Assets/Gustavo/Fireball molotov whatever this is/FireballFire.cs
@@ -7,13 +7,13 @@ public class MolotovThrow : MonoBehaviour
     public GameObject Molotov;
     public float cooldownTime = 5f;
     public float nextFireTime = 0f;
-    public float rotationSpeed = 100f;
+   
 
     void Update()
     {
         if (Input.GetMouseButton(0) && Time.time >= nextFireTime)
         {
-            transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+            
             Instantiate(Molotov, firePosition.position, firePosition.rotation);
             nextFireTime = Time.time + cooldownTime;
         }
