590eb45d122ce2adb623b789cae3732120799897
MUKILTEO\1725219 <george.chmykhalo@gmail.com>
2026-01-30 09:51:37 -0800

    minor changes, start of Game Manager

--- Assets/Tile-Editor/PlayerHandler.cs
+++ Assets/Tile-Editor/PlayerHandler.cs
@@ -1,15 +1,25 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerHandler : MonoBehaviour
 {
     public int playerCount =  0;
     public Sprite selectedSprite;
     public Sprite[] spriteOptions;
+    public PlayerInputManager managerinput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int i = 0; i < InputSystem.devices.Count; ++i)
+        {
+            var device = InputSystem.devices[i];
+
+            if (device.displayName == "Keyboard" || device.displayName == "Xbox Controller")
+            {
+                var input = managerinput.JoinPlayer(pairWithDevice: device);
+            }
+        }
     }
 
     // Update is called once per frame
