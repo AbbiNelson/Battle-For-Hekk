43e20b1b64b72aaac0e692babfe4b6adbd5d44dd
MUKILTEO\1630107 <1630107@mukilteo.wednet.edu>
2026-02-02 10:07:54 -0800

    Hitscan coming along

--- Assets/Abigail Folder/Scripts/HitScan.cs
+++ Assets/Abigail Folder/Scripts/HitScan.cs
@@ -2,5 +2,34 @@ using UnityEngine;
 
 public class HitScan : MonoBehaviour
 {
-    
+    [SerializeField] private Transform _gunPoint;
+    [SerializeField] private GameObject _bulletTrail;
+    [SerializeField] private float _weaponRange = 10f;
+    [SerializeField] private Animator _muzzleFlashAnimator;
+
+
+    private void Update()
+    {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _muzzleFlashAnimator.SetTrigger(name: "Shoot");
+
+            var hit = Physics2D.Raycast(
+                origin: (Vector2) _gunPoint.position,
+                direction: (Vector2) transform.up,
+                _weaponRange
+                );
+
+            var trail = Instantiate(
+                _bulletTrail,
+                _gunPoint.position,
+                transform.rotation
+                );
+        }
+    }
 }
