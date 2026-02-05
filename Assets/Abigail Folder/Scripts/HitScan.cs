dcc501c86b4d5e64bd9b8a48992e398b904993f6
MUKILTEO\1630107 <1630107@mukilteo.wednet.edu>
2026-02-05 07:53:35 -0800

    lpuh

--- Assets/Abigail Folder/Scripts/HitScan.cs
+++ Assets/Abigail Folder/Scripts/HitScan.cs
@@ -17,11 +17,11 @@ public class HitScan : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            _muzzleFlashAnimator.SetTrigger(name: "Shoot");
+            //_muzzleFlashAnimator.SetTrigger(name: "Shoot");
 
             var hit = Physics2D.Raycast(
                 origin: (Vector2) _gunPoint.position,
-                direction: (Vector2) transform.up,
+                direction: (Vector2) transform.right,
                 _weaponRange
                 );
 
@@ -30,6 +30,22 @@ public class HitScan : MonoBehaviour
                 _gunPoint.position,
                 transform.rotation
                 );
+
+            var trailScript = trail.GetComponent<Trail>();
+
+            if (hit.collider != null)
+            {
+                trailScript.SetTargetPosition(hit.point);
+                //var hittable = hit.collider.GetComponent<IHittable>();
+                //hittable?.Hit();
+            }
+            else
+            {
+                var endPosition = _gunPoint.position + transform.right * _weaponRange;
+                trailScript.SetTargetPosition(endPosition);
+            }
+
+            Debug.Log("Is Shooting");
         }
     }
 }
