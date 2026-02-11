c3df1f84a6c98af48959de4975d681a269446062
MUKILTEO\1725608 <talonlee44@gmail.com>
2026-02-11 09:56:22 -0800

    DFGHJK

---
+++ Assets/Talon/TalonScripts/GetCharacter.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class GetCharacter : MonoBehaviour
+{
+    public void LoadSceneAndKeepValue()
+    {
+        // Use this to save info, have to wait until we add multiple controller support in ChrSelect
+        //string P1 = player 1's character choice
+        //Then use if (All the players selected a character) then load the next scene
+    }
+}
